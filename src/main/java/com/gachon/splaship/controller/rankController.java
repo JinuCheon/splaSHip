c3450151653244e17b4e65802b89a0825d6de8a8
JinuCheon(깻묵) <woojin8787@gmail.com>
2022-10-14 15:21:38 +0900

    파이썬 실행 테스트

--- src/main/java/com/gachon/splaship/controller/rankController.java
+++ src/main/java/com/gachon/splaship/controller/rankController.java
@@ -1,13 +1,38 @@
 package com.gachon.splaship.controller;
 
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.PumpStreamHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @RestController
 public class rankController {
 
     @GetMapping(value = "/hello")
-    public String hello() {
-        return "hello controller";
+    public String hello() throws InterruptedException, IOException {
+        System.out.println("Python Call");
+        String[] command = new String[4];
+        command[0] = "python3";
+        command[1] = "test.py"; //가장 상위 디렉토리 기준 상대경로
+        command[2] = "10";
+        command[3] = "20";
+
+        CommandLine commandLine = CommandLine.parse(command[0]);
+        for (int i = 1, n = command.length; i < n; i++) {
+            commandLine.addArgument(command[i]);
+        }
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PumpStreamHandler pumpStreamHandler = new PumpStreamHandler(outputStream);
+        DefaultExecutor executor = new DefaultExecutor();
+        executor.setStreamHandler(pumpStreamHandler);
+        int result = executor.execute(commandLine);
+        System.out.println("result: " + result);
+        System.out.println("output: " + outputStream.toString());
+        return outputStream.toString();
     }
 }
---
+++ src/test/java/com/gachon/splaship/pythontest.java
@@ -0,0 +1,11 @@
+package com.gachon.splaship;
+
+import org.junit.jupiter.api.Test;
+
+public class pythontest {
+
+    @Test
+    void testPython() {
+//        psvm
+    }
+}
